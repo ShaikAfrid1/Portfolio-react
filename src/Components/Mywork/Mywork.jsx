21ecbfa900f3fdf4b62745cd769ceb013f8f0298
afridshaik <shaikafrid500@gmail.com>
2025-07-24 14:39:55 +0530

    mywork section completed

--- src/Components/Mywork/Mywork.jsx
+++ src/Components/Mywork/Mywork.jsx
@@ -1,11 +1,26 @@
 import "./Mywork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mywork_data from "../../assets/mywork_data";
+import arrow_icon from "../../assets/arrow_icon.svg";
+
 const Mywork = () => {
-  return <div className="mywork">
-    <div className="mywork-title">
+  return (
+    <div className="mywork">
+      <div className="mywork-title">
         <h1>My Latest Work</h1>
-        <img src="" alt="" />
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, idx) => {
+          return <img key={idx} src={work.w_img} alt="" />;
+        })}
+      </div>
+      <div className="mywork-showmore">
+        <p>Show More</p>
+        <img src={arrow_icon} alt="" />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Mywork;
