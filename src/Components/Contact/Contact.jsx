5a8b17bea4de71fd106d87c5a81fdff30fda9225
afridshaik <shaikafrid500@gmail.com>
2025-08-11 20:40:08 +0530

    contact added

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import "./Contact.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <h1>Get in Touch</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's talk</h1>
+          <p>
+            I'm currently avaliable to take on new projects, so feel free to
+            send me a message about anything that you want me to work on. You
+            can contact anytime.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
